b5fe536cdd689249d4e0970aed936f7209cc9977
basri1996 <daviti.basiladze475@law.tsu.edu.ge>
2024-05-20 01:03:54 +0400

    Movies-task

--- src/main.tsx
+++ src/main.tsx
@@ -13,5 +13,5 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <App />
       </Provider>
     </HashRouter>
-  //  </React.StrictMode>
+   </React.StrictMode>
 );
